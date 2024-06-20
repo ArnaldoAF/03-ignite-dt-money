132454b38f7a64e1f5e1d151d10074435a5b636f
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-06-20 09:45:59 -0300

    server e requisição API

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
 
+interface Transaction {
+    id: number;
+    description: string;
+    type: 'income' | 'outcome';
+    price: number;
+    category: string;
+    createdAt: string;
+}
+
 export function Transactions() {
+
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+    async function loadTransactions() {
+        const response = await fetch('http://localhost:3333/transactions', {
+            // mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        console.log(response)
+        const data = await response.json()
+        console.log(data);
+        setTransactions(data)
+    }
+
+    useEffect(() => {
+        loadTransactions()
+    }, [])
+
+
     return (
         <div>
             <Header />
@@ -13,6 +43,21 @@ export function Transactions() {
                 <SearchForm />
                 <TransactionTable>
                     <tbody>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighlight variant={transaction.type}>
+                                            R$ 12.000,00
+
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
                         <tr>
                             <td width="50%">Desenvolvimento de Software</td>
                             <td>
